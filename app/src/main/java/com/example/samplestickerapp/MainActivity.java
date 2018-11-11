9005a2a845a3b2abaac112f2ef1226246e34ba63
Anand Rai <kumar.anand.rai@gmail.com>
2018-11-11 20:25:12 +0530

    basic structure

---
+++ app/src/main/java/com/example/samplestickerapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.samplestickerapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_packs:
+                    Intent intent = new Intent();
+                    intent.setClass(getApplicationContext(), EntryActivity.class);
+                    startActivity(intent);
+
+                    return true;
+                case R.id.navigation_share:
+                    Toast.makeText(getApplicationContext(),"share",Toast.LENGTH_LONG).show();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
---
+++ app/src/main/java/com/example/samplestickerapp/SplashScreen.java
@@ -0,0 +1,53 @@
+package com.example.samplestickerapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class SplashScreen extends AppCompatActivity {
+    RelativeLayout l1;
+    LinearLayout l2;
+    Animation uptodown,downtoup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+
+
+        l1 =  findViewById(R.id.l1);
+        l2 =  findViewById(R.id.l2);
+        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        l1.setAnimation(uptodown);
+        l2.setAnimation(downtoup);
+
+        final Intent intent = new Intent();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                intent.setClass(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 3500l);
+    }
+}
